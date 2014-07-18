b514dc190bad5ffbf3c5bdd375b82430dbda823e
tmartin2089 <codeoverdose@gmail.com>
2014-07-17 20:19:25 -0500

    .amount now autofills with random values
    Next will do same to dropdown select

--- other.js
+++ other.js
@@ -3,25 +3,63 @@ $(document).ready(addLinks());
 
 
 function addLinks () {
-    for (var i=0, link; i<5; i++) {
+    for (var i=0, link; i<5; i++) { 
+	//when function addLinks is called, the loop 
+	//terminates at 5 - this is what onclick function()alert(i)
+	//would see
         link = document.createElement("a");
         link.innerHTML = "Link " + i;
-        link.onclick = function (num) {
-            return function () {
-                console.log(num);
-            };
+		//on each iteration of the loop, local variable num 
+		//will be set to current value of i 
+        link.onclick = function(num) {
+			return function(){
+            alert(num);
+			};
         }(i);
-		
-		//the above evalutes to link.onclick(i), but it's saving
-		//each instantiation 
-		
-		//for each iteration step the outer function literal 
-		//will evaluate to a new function object with its own 
-		//scope and local variable num, whose value is set to 
-		//the current value of i. As num is never modified, 
-		//it will stay constant over the lifetime of the 
-		//closure: The next iteration step doesn't overwrite 
-		//the old value as the function objects are independent.
         document.body.appendChild(link);
     }
 }
+
+// function addLinks () {
+    // for (var i=0, link; i<5; i++) {
+        // link = document.createElement("a");
+        // link.innerHTML = "Link " + i;
+        // link.onclick = function (num) {
+            // return function () {
+                // console.log(num);
+            // };
+        // }(i);
+		
+		// //the above evalutes to link.onclick(i), but it's saving
+		// //each instantiation 
+		
+		// //for each iteration step the outer function literal 
+		// //will evaluate to a new function object with its own 
+		// //scope and local variable num, whose value is set to 
+		// //the current value of i. As num is never modified, 
+		// //it will stay constant over the lifetime of the 
+		// //closure: The next iteration step doesn't overwrite 
+		// //the old value as the function objects are independent.
+        // document.body.appendChild(link);
+    // }
+// }
+
+//time to build autofill button
+
+
+function autofill(){
+	var array1= [];
+	for(var i=0; i<4; i++){
+		var randomnumber = Math.floor((Math.random() * 8000)+1);
+		array1[i] = randomnumber;
+	};
+	for(var x=0; x < array1.length; x++){
+		var elem = $('.amount')[x];  //returns DOM object unlike just $('#loanAmt1')
+		elem.value =array1[x];       //which would be a jquery object
+	};
+	console.log(array1);
+};
+
+// var select = Math.floor((Math.random() * 8)+1);
+// console.log(select);
+
