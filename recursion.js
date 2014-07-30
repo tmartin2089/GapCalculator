51d5a8eafbb9d709ecfdb4a8529160a237ed0d99
tmartin2089 <tmartin2089@gmail.com>
2014-07-30 17:49:32 -0500

    recursion attempt, works 55%

--- recursion.js
+++ recursion.js
@@ -1,19 +1,29 @@
+//a
+var overNeed = 1000;
 var needAid = [{type:5, amt:347},{type: 6, amt:522},{type:5, amt:347},{type: 6, amt:522}];
+//c
 var totalNeedaid = 1738;  //will need to sum up amt w/o including sacredAid 
-var overNeed = 1600;
 var revisedNeed = []
 
 function revise(a,b,c,d,e){
-	if(a <= 0 || c <= 0){   //base case  totalNeed is less than 
-        console.log(e);
+	if(a <= 0 || b >= d.length){   //base case  totalNeed is less than 
+        if(a < 0){
+			var remainder =(Math.abs(a));
+			console.log(d);
+			}
+			else{
+			console.log(d);
+			};
+			console.log(remainder);       //will need to re-add remainder to aid
+			console.log(a);				  //return outside of function - new fn to rebuild
 		}
 	else{
-		var k = d[b].amt;
-		a -= k;
-		c -= k;
-		e.push({amt:d[b].amt = 0, type:d[b].type});
-		console.log(k);
-		return revise(a,b+1,c,d,e);
+		a -= d[b].amt;
+		c -= d[b].amt;
+		d[b].amt = 0, 
+		console.log(d);
+		console.log(a);
+		return a + revise(a,b+1,c,d);
 		};
 }
 
