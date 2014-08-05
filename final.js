2107a9446f295fb4ea605943515f14cb5075527f
tmartin2089 <tmartin2089@gmail.com>
2014-08-05 13:25:04 -0500

    Revert "Revert "Code cleanup""
    This reverts commit 08f8d980b2f0fd05b72ae62de6e62a562ab9f50d.

--- final.js
+++ final.js
@@ -93,6 +93,7 @@ function checkCostNeed(){
 		console.log('Student is over cost by $' + overCost);
 	}
 	else{
+		var overCost = 0;
 		console.log('Student is under cost by $' + (costofAttend - totalAid));
 	};
 	//determines if stdt is over need & how much
@@ -128,15 +129,12 @@ function checkCostNeed(){
 	// };
  // };
  
- function revisions(){
+ //generates loansFirst and loansAmt arrays for reviseloansNeed()
+ function needloansArray(){
 	var result = sumitUp();
-	var overResult = checkCostNeed();
 	var typeAmt = result.typeAmt;
-	var overage = overResult.overNeed;
-	var underage = overResult.underNeed;
 	var typeAmtL = typeAmt.length;
 	var loansFirst = [];
-	var grantsSecond = [];
 	var loansAmt = 0;
 	//gather need based loans first
 	for(var x = 0; x<typeAmtL; x++){
@@ -144,57 +142,46 @@ function checkCostNeed(){
 			loansFirst.push(typeAmt[x]);
 		};
 	};
-	//next gather grants
-	// for(var i=0; i<typeAmtL; i++){
-		// if(typeAmt[x].type >= 7 && typeAmt[x].type <= 11){
-			// grantsSecond.push(typeAmt[x]);
-		// };
-	// };
 	for(var k=0; k<loansFirst.length; k++){
 		loansAmt += loansFirst[k].amt;
-	}
-	console.log('Total in need based loans is' + loansAmt);
-	function reviseloansNeed(overamt,end,total$,needArray){
-		if(overamt <= 0 || end >= needArray.length){   //base case  totalNeed is less than 
-        //at end of revise - if negative then too much was taken away
-		//this returns negative as positive remainder for re-adding
-			if(overamt < 0){
-				var remainder =(Math.abs(overamt));
-				var remainder1 = "tj did it";
-				console.log(needArray);
-				console.log('Student can keep $' + remainder);  //will need to re-add remainder to aid
-			}
-			else{
-				var remainOver = overamt;
-				var remainder1 = "tj did it";
-				console.log('To be taken from any grants $' + overamt);
-			};
-				console.log(overamt);				  //return outside of function - new fn to rebuild
-		}
-		else
-		{
-			overamt -= needArray[end].amt;
-			total$ -= needArray[end].amt;
-			needArray[end].amt = 0; 
-			return overamt + reviseloansNeed(overamt,end+1,total$,needArray);
-		};
-		return{
-		remainder1: remainder1,
-		}
-	}
-	var testTj = reviseloansNeed(overage,0,loansAmt,loansFirst);
-	var itWorks = testTj.remainder1;
-	console.log(itWorks);
+	};
+	//var testTj = reviseloansNeed(overage,0,loansAmt,loansFirst);
 	return{
-	itWorks:itWorks,
+	loansFirst: loansFirst,
+	loansAmt: loansAmt,
 	};
  }
 
+function reviseloansNeed(overamt,end,total$,needArray){
+	if(overamt <= 0 || end >= needArray.length){   //base case  totalNeed is less than 
+    //at end of revise - if negative then too much was taken away
+	//this returns negative as positive remainder for re-adding
+		if(overamt < 0){
+			var remainder =(Math.abs(overamt));
+			console.log(needArray);
+			console.log('Student can keep $' + remainder);  //will need to re-add remainder to aid
+		}
+		else{
+			var remainOver = overamt;
+			console.log('To be taken from any grants $' + overamt);
+			console.log(needArray);
+		};
+	}	
+	else{
+		overamt -= needArray[end].amt;
+		total$ -= needArray[end].amt;
+		needArray[end].amt = 0; 
+		return overamt + reviseloansNeed(overamt,end+1,total$,needArray);
+	};
+}
+ 
 
  function doEverything(){
-	checkCostNeed();
-	revisions();
-	var testItWorks = revisions();
-	console.log(testItWorks.itWorks);
+	var overResult = checkCostNeed();
+	var needArray = needloansArray();
+	var loansAmt = needArray.loansAmt;
+	var loansFirst = needArray.loansFirst;
+	var overage = overResult.overNeed;
+	reviseloansNeed(overage,0,loansAmt,loansFirst);
  }
 
