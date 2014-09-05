b9ad6ffea364aac969212041512e555432dff776
tmartin2089 <tmartin2089@gmail.com>
2014-09-05 17:41:04 -0500

    Copy/paste of p6 now converts for award type
    lots to do oh my

--- copypaste.js
+++ copypaste.js
@@ -1,3 +1,7 @@
+//todo: remove 342000 from array p6 osch code
+//todo: reset all named scholarships to 331000
+//todo: reset all gms/terry to 341000
+
 //gets rid of all extraneous and returns numbers only
 function getPaste(){
 	var pasted = $('.paste').val().replace(/_/g,' ');
@@ -18,7 +22,8 @@ function separateType(){
 		if(getArray[x].toString().length === 6)
 			type.push(getArray[x]);
 	};
-	return type;
+	console.log(type);
+	convertType(type);
 }
 
 function separateAmt(){
@@ -42,6 +47,125 @@ function pastedtoObject(){
 	return typeAmt;
 }
 
+function convertType(val){
+	for(var x= 0; x < val.length; x++){
+		switch(val[x]){
+		
+		//plus
+		case 215025:
+		case 215026:
+		val[x] = 1;
+		break;
+		
+		//unsub
+		case 214125:
+		case 215126:
+		val[x] = 2;
+		break;
+		
+		//cal/alt
+		case 222001:
+		case 241002:
+		val[x] = 3;
+		break;
+		
+		//bot
+		case 223001:
+		case 223002:
+		case 223003:
+		case 223004:
+		val[x] = 4;
+		break;
+		
+		//perkins
+		case 213003:
+		val[x] = 5;
+		break;
+		
+		//sub
+		case 214025:
+		case 214026:
+		val[x] = 6;
+		break;
+		
+		//UT grant	
+		case 131252:
+		case 131253:
+		case 131254:
+		case 131255:
+		case 131256:
+		val[x] = 7;
+		break;
+		
+		//PTG
+		case 131200:
+		case 131201:
+		case 131202:
+		val[x] = 8;
+		break;
+		
+		//TPEG
+		case 131115:
+		case 131110:
+		case 131116:
+		case 131111:
+		val[x] = 9;
+		break;
+		
+		//tx grant match
+		case 131114:
+		val[x] = 10;
+		break;
+		
+		//tx grant
+		case 121176:
+		case 121178:
+		case 121180:
+		val[x] = 11;
+		break;
+		
+		//fws
+		case 411001:
+		val[x] = 12;
+		break;
+		
+		//pell
+		case 111028:
+		case 111029:
+		val[x] = 13;
+		break;
+		
+		//top 10
+		case 321380:
+		case 321396:
+		val[x] = 14;
+		break;
+		
+		//need to add in named - convert to 331000
+		case 331000:
+		case 331000:
+		val[x] = 15;
+		break;
+		
+		//catchall for gms/terry - convert to 341000
+		case 341000:
+		val[x] = 16;
+		break;
+		
+		
+		default:
+		console.log('nothing here boss');
+		break;
+		}
+	};	
+	
+	console.log('convertype is now working');
+	console.log(val);
+	return val;
+	console.log(typeof val);
+};
+	
+	
 
 
 
