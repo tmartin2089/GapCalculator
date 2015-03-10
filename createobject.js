f15894447091ce0010c92fb8c37f35f3aaaba3d2
TJ Martin <codeoverdose@gmail.com>
2015-03-10 10:18:16 -0500

    misc ajax

--- createobject.js
+++ createobject.js
@@ -16,6 +16,29 @@
 
 "use strict";
 
+
+$('#calcp6').keyup(function() {
+	console.log("Ajax stuff is happening here sire");
+	var searchField = $('#search').val();
+	var myExp = new RegExp(searchField, "i");
+	$.getJSON('datafile.json', function(data) {
+		var output = '<ul class="searchresults">';
+		$.each(data, function(key, val) {
+			if ((val.name.search(myExp) != -1) ||
+			(val.bio.search(myExp) != -1)) {
+				output += '<li>';
+				output += '<h2>'+ val.name +'</h2>';
+				output += '<img src="images/'+ val.shortname +'_tn.jpg" alt="'+ val.name +'" />';
+				output += '<p>'+ val.bio +'</p>';
+				output += '</li>';
+			}
+		});
+		output += '</ul>';
+		$('#update').html(output);
+	}); //get JSON
+});
+
+
 var doAidmagic = (function(){
 
 	// ^1 -----------------------------Useful Higher Order Variables------------------------------------
